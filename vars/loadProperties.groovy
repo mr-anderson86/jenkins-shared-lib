b0f9f5945e1f1d562731c08acbf79e6fca5aa31e
Mr. Anderson 86 <39731910+mr-anderson86@users.noreply.github.com>
2020-02-13 12:56:08 +0200

    Create loadProperties.groovy

---
+++ vars/loadProperties.groovy
@@ -0,0 +1,22 @@
+def call(String path) {
+    /*Loads properties from file into env map
+    (Both in pipeline and in Shell)
+    Properties file needs to be at this structure:
+    VAR_1=Something
+    var_2="somthing"
+    var_3='something'
+    var_4=1234
+    var_5=1.2.3
+    var_6="1.2.3"
+    And os on...
+    */
+    
+    def props = readFile path
+    //Debugging
+    //echo props
+    props.split('\n').each { prop ->
+        String key = (String)prop.split('=')[0]
+        value = prop.split('=')[1]
+        env."${key}" = value
+    }
+}
