236c8fb35ae1def85378292b3c802c49fc546af9
Mr. Anderson 86 <39731910+mr-anderson86@users.noreply.github.com>
2020-02-16 16:13:37 +0200

    Create parseJson.groovy

---
+++ vars/parseJson.groovy
@@ -0,0 +1,14 @@
+def call(String text) {
+  /*Gets json as text,
+    return it as map (using jsonSlurper: https://groovy-lang.org/json.html)
+    jsonSlurper also puts the data within the map as: string, number, array, boolean, null 
+    and date (based on the yyyy-MM-dd’T’HH:mm:ssZ date format)
+    
+    Might need admin script appoval
+  */
+  
+  def jsonSlurper = new JsonSlurper()
+  def data = jsonSlurper.parseText(text)
+  assert data instanceof Map
+  return data
+}
