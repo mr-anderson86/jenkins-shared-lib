4b1e2d018f599376b8c195e8b66c0ac4918e5595
Mr. Anderson 86 <39731910+mr-anderson86@users.noreply.github.com>
2020-02-16 17:18:24 +0200

    Update parseJson.groovy

--- vars/parseJson.groovy
+++ vars/parseJson.groovy
@@ -1,20 +1,27 @@
 import groovy.json.JsonSlurper
 import static groovy.json.JsonOutput.*
-def call(String text) {
-  /*Gets json as text,
-    return it as map (using jsonSlurper: https://groovy-lang.org/json.html)
-    jsonSlurper also puts the data within the map as: string, number, array, boolean, null 
-    and date (based on the yyyy-MM-dd’T’HH:mm:ssZ date format)
-    
-    Might need admin script appoval
-  */
+
+/*
+  INPUT: json as string
+  RETURN: data as map (using jsonSlurper: https://groovy-lang.org/json.html)
   
+  jsonSlurper also puts the data within the map as: string, number, array, boolean, null 
+  and date (based on the yyyy-MM-dd’T’HH:mm:ssZ date format)
+  
+  PLEASE NOTE: Might need admin script appoval (unless using as shared library)
+*/
+def call(String text) {  
   def jsonSlurper = new JsonSlurper()
   def data = jsonSlurper.parseText(text)
   assert data instanceof Map
   return data
 }
 
+/*
+  INPUT: Map or json (in text)
+  OUTPUT: in json it'll print in one line
+          in map it'll print with indentation
+*/
 def prettyPrint(data) {
   echo prettyPrint(toJson(data))
 }
