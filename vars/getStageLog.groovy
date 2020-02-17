80b1a7a4b8496a7daf1687080f441f3ec67ffb42
Mr. Anderson 86 <39731910+mr-anderson86@users.noreply.github.com>
2020-02-17 17:30:05 +0200

    Update getStageLog.groovy

--- vars/getStageLog.groovy
+++ vars/getStageLog.groovy
@@ -1,14 +1,22 @@
+/*
+  PLEASE NOTE: Might need admin script appoval (unless using as shared library)
+  (Only because of currentBuild.rawBuild.getLog)
+  I could've use currentBuild.rawBuild.log and skip the approval part, but it contains all those "(hide)" links, 
+  which in mail or file looks very messy.
+  The getLog method is the only way I found to get the log without those messy links. :/ 
+*/
+
+
+/**
+ * @param stageName, String
+ * @return log of stage only, as String
+ *         if stage isn't finished yet, then it'll return stage's log up to function calling point.
+ *         if no stage name was given, or stage name was not found,
+ *         Then it'll return the whole build log up to function calling step.
+ *
+ * @usage example: def stageLog = getStageLog(env.STAGE_NAME)
+ */
 def call(String stageName = '') {
-	/*Return spesific stage log in current build.
-	  if no stage name was given, or stage name was not found,
-	  Then it'll return the whole build log up to function calling step.
-	*/
-	
-	/*The getLog method below requiers Admin Script Approval.
-	  I could've use currentBuild.rawBuild.log and skip the approval part, but it contains all those "(hide)" links, 
-	  which in mail or file looks very messy.
-	  The getLog method is the only way I found to get the log without those messy links. :/
-	*/
 	def buildLog = currentBuild.rawBuild.getLog(currentBuild.rawBuild.log.length()).join('\n')
 	//println buildLog
 
