67a4166afb06fc1c2d6dec4093b94f30324c92d6
Mr. Anderson 86 <39731910+mr-anderson86@users.noreply.github.com>
2020-02-17 15:45:18 +0200

    Update parseJson.groovy

--- vars/parseJson.groovy
+++ vars/parseJson.groovy
@@ -1,31 +1,51 @@
 import groovy.json.JsonSlurper
 import static groovy.json.JsonOutput.*
+//PLEASE NOTE: Might need admin script appoval (unless using as shared library)
 
-/*
-  INPUT: json as string
-  RETURN: data as map (using jsonSlurper: https://groovy-lang.org/json.html)
-  
-  jsonSlurper also puts the data within the map as: string, number, array, boolean, null 
-  and date (based on the yyyy-MM-dd’T’HH:mm:ssZ date format)
-  
-  PLEASE NOTE: Might need admin script appoval (unless using as shared library)
-*/
-def call(String text) {  
+/**
+ * Returns json string as a Map using jsonSlurper: https://groovy-lang.org/json.html ).
+ * jsonSlurper also puts the data within the map as: string, number, array, boolean, null 
+ * and date (based on the yyyy-MM-dd’T’HH:mm:ssZ date format) 
+ *
+ * @param json, String in json format
+ * @return data, as map
+ *
+ * @usage example: def myMap = parseJson('{ "name": "John Doe" }')
+ */
+def call(String json) {  
   def jsonSlurper = new JsonSlurper()
-  def data = jsonSlurper.parseText(text)
+  def data = jsonSlurper.parseText(json)
   assert data instanceof Map
   return data
 }
 
-/*
-  INPUT: Map or json (in text)
-  OUTPUT: in json it'll print in one line
-          in map it'll print with indentation
-*/
+/**
+ * @param data, Map or String in json format
+ * @output if data is json (string), then it'll print in one line
+ *         if data is map, then it'll print with indentation
+ *
+ * @usage example: parseJson.prettyPrint(data)
+ */
 def prettyPrint(data) {
   echo prettyPrint(toJson(data))
 }
 
+/**
+ * @param data, the data in Map
+ * @return String in json format
+ *
+ * @usage example: def json = parseJson.mapToJson(data)
+ */
 def mapToJson(Map data) {
   return toJson(data)
+}
+
+/**
+ * @param url, in a string
+ * @return the json from the URL as a map
+ *
+ * @usage example: def myMap = parseJson.fromUrl("${BUILD_URL}/api/json")
+ */
+def fromUrl(String url) {
+  return new JsonSlurper().parseText(new URL(url))
 }
