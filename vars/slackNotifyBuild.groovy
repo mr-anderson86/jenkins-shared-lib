5db3f9f03fef4973d42d164501c85a7372dcdb8e
Mr. Anderson 86 <39731910+mr-anderson86@users.noreply.github.com>
2020-04-26 19:42:06 +0300

    Update slackNotifyBuild.groovy

--- vars/slackNotifyBuild.groovy
+++ vars/slackNotifyBuild.groovy
@@ -14,7 +14,7 @@
 
 def call(String buildStatus = 'STARTED') {
   // build status of null means successful
-  buildStatus =  buildStatus ?: 'SUCCESSFUL'
+  buildStatus =  buildStatus ?: 'SUCCESS'
 
   // Default values
   def colorName = 'RED'
@@ -24,11 +24,17 @@ def call(String buildStatus = 'STARTED') {
 
   // Override default values based on build status
   if (buildStatus == 'STARTED') {
-    color = 'YELLOW'
-    colorCode = '#FFFF00'
-  } else if (buildStatus == 'SUCCESSFUL') {
+    color = 'BLUE'
+    colorCode = '#3385ff'
+  } else if (buildStatus == 'SUCCESS') {
     color = 'GREEN'
     colorCode = '#00FF00'
+  } else if (buildStatus == 'UNSTABLE') {
+    color = 'YELLOW'
+    colorCode = '#FFFF00'
+  } else if (buildStatus == 'ABORTED' || buildStatus == 'NOT_BUILT') {
+    color = 'GREY'
+    colorCode = '#808080'
   } else {
     color = 'RED'
     colorCode = '#FF0000'
