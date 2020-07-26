cf4bb9fee7ac90c53356ed968f0589a1b34af9c3
Mr. Anderson 86 <39731910+mr-anderson86@users.noreply.github.com>
2020-07-26 23:48:05 +0300

    Create buildJobsFromFile.groovy

---
+++ vars/buildJobsFromFile.groovy
@@ -0,0 +1,34 @@
+/**
+ * Read job names from a file, and triggers them all in parallel
+ *
+ * @param String jobsFile (required)
+ * @param List<params> parameters (optional)
+ * @usage example:
+ *        buildJobsFromFile(jobsFile: 'triggers.txt')
+ *        buildJobsFromFile(jobsFile: 'triggers.txt', parameters: currentBuild.rawBuild.getAction(ParametersAction).getParameters())
+ */
+
+def call(String jobsFile, parameters = []) {
+    file = readFile jobsFile
+    lines = file.readLines()
+    builds = [:]
+    jobs = []
+    if(lines != null){
+        buildParams = parameters
+        int i = 0
+        for(job in lines){
+            jobs << job
+            //echo "build job: ${jobs[i]}, parameters: ${buildParams}"
+            builds["Job_" + job] = {
+                build job: jobs[i++], parameters: buildParams
+            }
+        }
+        parallel builds
+    }
+}
+
+def call(Map config) {
+    if (!config.containsKey('jobsFile')) { throw new Exception("Method 'buildJobsFromFile' must contain param 'jobsFile'!") }
+    if (!config.containsKey('parameters')) {config.parameters = []}
+    call(config.jobsFile, config.parameters)
+}
