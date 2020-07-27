b7c2646e4f977f6e9c72bc24bf31ebabe15eb9ce
Mr. Anderson 86 <39731910+mr-anderson86@users.noreply.github.com>
2020-07-27 14:37:04 +0300

    Update loadProperties.groovy

--- vars/loadProperties.groovy
+++ vars/loadProperties.groovy
@@ -2,7 +2,6 @@
  * Loads properties from file into env map
  * (Both in pipeline and in Shell)
  * Properties file needs to be at this structure:
- * 
  * VAR_1=Something
  * var_2="somthing"
  * var_3='something'
@@ -10,15 +9,16 @@
  * var_5=1.2.3
  * var_6="1.2.3"
  * And os on...
- * @return void
  *
+ * @param String filePath
+ * @return void
  * @usage examples:
  *        loadProperties('path/to/file')
  *        loadProperties(filePath: 'path/to/file')
  */
 
-def call(String path) {    
-    def props = readFile path
+def call(String filePath) {    
+    def props = readFile filePath
     //Debugging
     //echo props
     props.split('\n').each { prop ->
