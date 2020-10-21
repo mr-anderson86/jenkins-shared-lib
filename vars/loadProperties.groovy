0e7b455f8760cd7b9532344d409a52ada083dbe9
Mr. Anderson 86 <39731910+mr-anderson86@users.noreply.github.com>
2020-10-21 15:09:45 +0300

    Update loadProperties.groovy

--- vars/loadProperties.groovy
+++ vars/loadProperties.groovy
@@ -5,29 +5,62 @@
  * VAR_1=Something
  * var_2="somthing"
  * var_3='something'
+ * 
+ * #commented and empty lines will be skipped
+ * //commented line will be skipped
  * var_4=1234
  * var_5=1.2.3
  * var_6="1.2.3"
- * And os on...
  *
- * @param String filePath
- * @return void
+ * @param String file
+ * @param Boolean toVar (optional)
+ * @return void or map, depends if toVar is true/false
  * @usage examples:
- *        loadProperties('path/to/file')
- *        loadProperties(filePath: 'path/to/file')
+ *        Into env:
+ *          loadProperties(file: "/path/to/file")
+ *          echo env.myKey
+ *
+ *        into var:
+ *          def myMap = loadProperties(file: "/path/to/file", toVar: true)
+ *          echo myMap.myKey
  */
 
-def call(String filePath) {    
-    def props = readFile filePath
-    //Debugging
-    //echo props
+def call(String path, Boolean toVar = false) {
+  def props = readFile path
+  //Debugging
+  //echo props
+  if (!toVar) {
     props.split('\n').each { prop ->
+      if (!isLineSkippable((String)prop)) {
         String key = (String)prop.split('=')[0]
         value = prop.split('=')[1]
         env."${key}" = value
+      }
     }
+  } else {
+    Map m = [:]
+    props.split('\n').each { prop ->
+      if (!isLineSkippable((String)prop)) {
+        String key = (String)prop.split('=')[0]
+        value = prop.split('=')[1]
+        m."${key}" = value
+      }
+    }
+    return m
+  }
 }
 
 def call(Map config) {
-  call(config.filePath)
+  if (!config.containsKey('file')) {
+    error("Must provide 'file', exmaple - loadProperties(file: '/path/to/file')")
+  }
+  if (config.toVar) {
+    return call(config.file, config.toVar)
+  } else {
+    call(config.file, config.toVar)
+  }
+}
+
+def isLineSkippable(String line) {
+    return (line.replaceAll(' ','').replace("\t", "").trim().isEmpty() || line.matches("(#|//).*"))
 }
